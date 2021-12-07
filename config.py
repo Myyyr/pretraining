6e8b06cdda9635a2f4283dacf7b5c9891de96073
MYR <loic.themyr@gmail.com>
2021-12-07 17:29:13 +0100

    pret

--- config.py
+++ config.py
@@ -219,6 +219,8 @@ def update_config(config, args):
     if args.resume:
         config.MODEL.RESUME = args.resume
     if args.accumulation_steps:
+        print("\n\n\n\n\nPARSER",args.accumulation_steps,"\n\n\n\n\n")
+        exit(0)
         config.TRAIN.ACCUMULATION_STEPS = args.accumulation_steps
     if args.use_checkpoint:
         config.TRAIN.USE_CHECKPOINT = True
