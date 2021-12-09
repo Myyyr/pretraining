d2581d55a22ef2e087f7e79590d58afc4bac7e40
MYR <loic.themyr@gmail.com>
2021-12-09 14:41:20 +0100

    pret

--- multi_slurm_runner.sh
+++ multi_slurm_runner.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
+# get_last () {
+#     folder="files"
+#     let last=0
+#     for f in "$folder"/*
+#     do
+#         ext=${f##*.}
+#         if [ "$ext" = "pth" ]; then
+#             tmp=${f%.*}
+#             tmp=${tmp##*_}
+#             if [[ $tmp > $last ]]; then
+#                 let last=tmp
+#             fi
+#         fi
+#     done
+#     echo $last
+# }
+# ret="$(get_last)"
+# echo "Last: $ret"
 
-jid=$(sbatch slurm.sh | cut -d ' ' -f4)
-echo "jid: $jid"
+
+# jid=$(sbatch slurm.sh | cut -d ' ' -f4)
+sbatch $1
+# echo "jid: $jid"
 
 for k in {0..1};
     do temp="${k}"
         # jid=$(sbatch --dependency=afterok:${jid} slurm_resume.sh ${k} | cut -d ' ' -f4)
-        jid=$(sbatch --dependency=singleton slurm_resume.sh ${k} | cut -d ' ' -f4)
-    	echo "jid: $jid"
+        # jid=$(sbatch --dependency=singleton slurm.sh --resume | cut -d ' ' -f4)
+        sbatch --dependency=singleton $1 --resume
     done
--- slurm_swin.sh
+++ slurm_swin.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=00:15:00              # maximum execution time (HH:MM:SS)
+#SBATCH --time=00:00:30              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-dev
 #SBATCH --output=logs/swin%j.out # output file name
 #SBATCH --error=logs/swin%j.err  # error file name
@@ -24,11 +24,8 @@ CONFIG="configs/swin_tiny_patch4_window7_224.yaml"
 # CONFIG="configs/glam_segformer_patch4_window7_224.yaml"
 
 
-# RESUME="output/swin_tiny_patch4_window7_224/default/ckpt_epoch_$1.pth"
-
-
 # swin
-srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u  main.py --cfg $CONFIG --data-path $DATA --batch-size 128 --local_rank 0 --resume #$RESUME
+srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u  main.py --cfg $CONFIG --data-path $DATA --batch-size 128 --local_rank 0 $1
 # srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u  main.py --cfg $CONFIG --data-path $DATA --batch-size 32 --local_rank 0 --accumulation-steps 4 # --use-checkpoint
 # srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u -m torch.distributed.launch --nproc_per_node 8 --master_port 12345  main.py --cfg $CONFIG --data-path $DATA --batch-size 128
 # srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u tools/train.py $CONFIG --options model.pretrained=$PRET --launcher="slurm" --seed 0 --deterministic ${@:3}
