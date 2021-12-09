82ebbf04fe64720bbf64a272766461484e4236f7
MYR <loic.themyr@gmail.com>
2021-12-09 11:34:27 +0100

    pret

--- multi_slurm_runner.sh
+++ multi_slurm_runner.sh
@@ -2,9 +2,10 @@
 
 
 jid=$(sbatch slurm.sh | cut -d ' ' -f4)
-
+echo "jid: $jid"
 
 for k in {0..3};
     do temp="${k}"
         jid=$(sbatch --dependency=afterok:${jid} slurm_resume.sh ${k} | cut -d ' ' -f4)
+    	echo "jid: $jid"
     done
