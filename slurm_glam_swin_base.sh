70e938aac4d63979803995171d9296a7b5d45184
MYR <loic.themyr@gmail.com>
2021-12-15 18:04:47 +0100

    pret

--- slurm_glam_swin_base.sh
+++ slurm_glam_swin_base.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=00:02:30              # maximum execution time (HH:MM:SS)
+#SBATCH --time=00:30:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-dev
 #SBATCH --output=logs/glamswinbase%j.out # output file name
 #SBATCH --error=logs/glamswinbase%j.err  # error file name
