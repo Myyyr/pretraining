da1535c0c65d3306405f22933db55dae9084e316
MYR <loic.themyr@gmail.com>
2022-01-06 16:19:57 +0100

    pret

--- slurm_glam_swin_base.sh
+++ slurm_glam_swin_base.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=glamswinbase     # job name
+#SBATCH --job-name=vglamswinbase     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=19:59:59              # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/glamswinbase%j.out # output file name
-#SBATCH --error=logs/glamswinbase%j.err  # error file name
+#SBATCH --time=00:05:59              # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-dev
+#SBATCH --output=logs/vglamswinbase%j.out # output file name
+#SBATCH --error=logs/vglamswinbase%j.err  # error file name
+#SBATCH -C v100-32g
 
 set -x
 
@@ -20,10 +21,13 @@ module load python/3.7.10
 
 
 DATA="/gpfsscratch/rech/arf/unm89rb/imagenet"
-CONFIG="configs/glam_swin_base_patch4_window7_224.yaml"
-# CONFIG="configs/glam_segformer_patch4_window7_224.yaml"
 
 
 # swin
-# srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u  main.py --cfg $CONFIG --data-path $DATA --batch-size 128 --local_rank 0 $1 $2
-srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u  main.py --cfg $CONFIG --data-path $DATA --batch-size 64 --local_rank 0 --accumulation-steps 2 $1 $2
+
+# CONFIG="configs/glam_swin_base_patch4_window7_224.yaml"
+# srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u  main.py --cfg $CONFIG --data-path $DATA --batch-size 64 --local_rank 0 --accumulation-steps 2 $1 $2
+
+
+CONFIG="configs/glam_swin_base_patch4_window7_224_bis.yaml"
+srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u  main.py --cfg $CONFIG --data-path $DATA --batch-size 128 --local_rank 0 --use-checkpoint $1 $2 
